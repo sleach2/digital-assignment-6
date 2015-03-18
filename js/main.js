e93e33b23d7bd92b7c15c3631058d9732aa5704c
sleach2 <sleach2@gmu.edu>
2015-03-18 19:50:21 -0400

    b

--- js/main.js
+++ js/main.js
@@ -4,22 +4,38 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.spritesheet( 'player', 'assets/dude.png', 32, 48 );
     }
     
-    var bouncy;
+    var player;
     
     function create() {
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        bouncy.body.collideWorldBounds = true;
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        game.world.setBounds(0,0,2000,2000);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        player = game.add.sprite(200, game.world.height - 90, 'dude');
+        game.physics.arcade.enable(player);
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        cursors = game.input.keyboard.createCursorKeys();
+        game.camera.follow(player);
     }
     
     function update() {
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        player.body.velocity.x=0;
+        if (cursors.left.isDown){
+            player.body.velocity.x = -250;
+            player.animations.play('left');
+        }else if (cursors.right.isDown){
+            player.body.velocity.x = 250;
+            player.animations.play('right');
+        }else{
+            player.animations.stop();
+            player.frame = 4;
+        }
+        if (cursors.up.isDown && player.body.touching.down){
+            player.body.velocity.y = -350;
+        }
     }
 };
