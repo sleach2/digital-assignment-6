92178dac3f911fe1d5a746420806810e2626a32b
sleach2 <sleach2@gmu.edu>
2015-03-24 19:04:13 -0400

    d

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ window.onload = function() {
     
     function update() {
         player.body.velocity.x=0;
+        player.body.velocity.y=350;
         if (cursors.left.isDown){
             player.body.velocity.x = -250;
             player.animations.play('left');
